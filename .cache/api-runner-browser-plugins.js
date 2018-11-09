85ff4009b9c8fd4beebecbb83100fda6564ff7d4
Yixuan Xu <yixuanxu94@gmail.com>
2018-11-10 00:43:26 +0800

    add config

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/xuyixuan/Desktop/homeless/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/xuyixuan/Desktop/homeless/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,10 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/Users/xuyixuan/Desktop/homeless/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-pages-index-js": () => import("/Users/xuyixuan/Desktop/homeless/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
+exports.data = () => import("/Users/xuyixuan/Desktop/homeless/.cache/data.json")
+
