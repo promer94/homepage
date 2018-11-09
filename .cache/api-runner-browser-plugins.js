4e5fee2abc80e79428aaaad233bc4630f70d438a
Yixuan Xu <yixuanxu94@gmail.com>
2018-11-10 01:17:30 +0800

    :fire: Remove

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/xuyixuan/Desktop/homeless/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/xuyixuan/Desktop/homeless/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/sync-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-
-exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/xuyixuan/Desktop/homeless/node_modules/gatsby-plugin-offline/app-shell.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/xuyixuan/Desktop/homeless/src/pages/index.js"))
-}
-
